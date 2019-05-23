8ba615d237b9972dc1e030f5abc98b44237bc462
Masterflash <sxq@yandex.ru>
2019-05-23 16:26:33 +0300

    n

--- src/Service/Zform/Plugin/Factory/Locale.php
+++ src/Service/Zform/Plugin/Factory/Locale.php
@@ -13,6 +13,9 @@ class Locale
 public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
 {
 	$config=$container->get('config');
+    if (empty($config["locale_enable_list"])){
+        $config["locale_enable_list"]=[$config["locale_default"]];
+    }
     return new $requestedName($config["locale_enable_list"]);
 }
 }
