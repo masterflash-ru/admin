f65dbfec72a40163813c7cf9685a219d739d8136
Masterflash <sxq@yandex.ru>
2019-06-25 15:18:32 +0300

    Update admin.admin_menu.php

--- config/admin.admin_menu.php
+++ config/admin.admin_menu.php
@@ -108,6 +108,7 @@ return [
                                                       ],
                                                   ]),
                     ColModelHelper::hidden("id"),
+                    ColModelHelper::cellActions("myaction",["formatoptions"=>["editformbutton"=>true]]),
                 ],
             ],
         ],
