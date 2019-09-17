d51ae39d9520432545b6c311d1c5cb5fd12f21de
Masterflash <sxq@yandex.ru>
2019-09-17 11:34:25 +0300

    Update F51.php

--- src/Lib/Fhelper/F51.php
+++ src/Lib/Fhelper/F51.php
@@ -22,44 +22,6 @@ public function __construct($item_id)
 	
 
     
-/*обработка записи
-
-$this->id - ID записи основной таблицы (товара)
-*/
-public function save()
-{
-    
-//    \Zend\Debug\Debug::dump($_POST[$this->col_name][$this->id]);
-    if (!is_array($_POST[$this->col_name][$this->id])){
-        return;
-    }
-    //удалим старое
-    simba::query("delete from tovar_parameters_value where tovar_catalog=".(int)$this->id);
-    foreach ($_POST[$this->col_name][$this->id] as $tovar_parameters_id=>$value){
-        //заносим вновь новые
-        if (is_array($value)){
-            //запись массива
-            foreach ($value as $v){
-                if (!$v){continue;}
-                Simba::ReplaceRecord([
-                    "tovar_catalog"=>(int)$this->id,
-                    "tovar_parameters"=>(int)$tovar_parameters_id,
-                    "value"=>$v
-                    ],"tovar_parameters_value");
-            }
-        } else {
-            if (!$value){continue;}
-            Simba::ReplaceRecord([
-                "tovar_catalog"=>(int)$this->id,
-                "tovar_parameters"=>(int)$tovar_parameters_id,
-                "value"=>$value
-                ],"tovar_parameters_value");
-        }
-    }
-    
-    
-}
-    
     
     
 /*рендер элемента в админке*/	
@@ -75,7 +37,6 @@ public function render()
 $out=<<<EOT
 <label>Выберите марку: <select name="avto_brend" id="avto_brend"></select></label><br/>
 <label>Выберите модель: <select name="avto_model" id="avto_model"></select></label>
-<input type="hidden" value="{VALUE}" id="brend_model" name="{NAME0}" >
 <script>
 $(function() {
 var start_avto_brend=0,start_avto_model=0;
@@ -84,7 +45,7 @@ var start_avto_brend=0,start_avto_model=0;
 $("#avto_brend").change(function(e) {
         var start=start_avto_brend, avto_brend=0;
 		if (start>0) {avto_brend=start;} else {avto_brend=$(this).val();}
-        $("#avto_model").load("/admin/ajax",{type:"load_avto_model",avto_brend:avto_brend},
+        $("#avto_model").load("/aajax",{type:"load_avto_model",avto_brend:avto_brend},
                 function()
                     {
 					start_avto_brend=0;
@@ -99,7 +60,7 @@ $("#avto_brend").change(function(e) {
 });
 
 //загрузка марок
-$("#avto_brend").load("/admin/ajax",{type:"load_avto_brend"},function()
+$("#avto_brend").load("/aajax",{type:"load_avto_brend"},function()
     {
 		var starts=$("#brend_model").val();
 		starts=starts.split(",");
@@ -126,7 +87,7 @@ $("#avto_model").change(function(e) {
 EOT;
 
     
-    return $out;
+    return $out.'<input type="hidden" value="'.$this->value.'" id="brend_model" name="'.$name.'" >';
 }
 
 
